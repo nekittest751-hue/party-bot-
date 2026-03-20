e859809162798c2883ef34f25fedc558904dc72e
nekittest751-hue <nekittest751@gmail.com>
2026-03-20 23:10:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -163,3 +163,20 @@ BUSINESS (1999₽):
 # ЗАПУСК
 # =====================
 bot.infinity_polling()
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_bot():
+    bot.infinity_polling()
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
